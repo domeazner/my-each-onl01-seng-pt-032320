00c5cd44ce6f35de935dd289b2f4e7044bfa8803
Dominc Carolan <dominiccarolan@gmail.com>
2020-03-31 20:13:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
-my_each(collection)
+def my_each(collection)
   i = 0
   while i < collection.length
-yield(collection[i])
-i = i + 1
-end
-collection
+  yield(collection[i])
+  i = i + 1
+  end
+  collection
 end
